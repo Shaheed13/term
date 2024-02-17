a6e960057a359f1913be75c6841ed77da811b918
Shaheed13 <109297643+Shaheed13@users.noreply.github.com>
2024-02-17 10:33:38 +0000

    vfhb

---
+++ basics/strings.js
@@ -0,0 +1,30 @@
+const name =new String('shaheedjdhkiashdkahdkj');
+const age = 13452;
+//console.log(user + userId +"value");
+
+//literals//
+
+
+console.log(`mynahsjhasjej ${name} my namenmndm ${age}`);
+
+console.log(name[0]);
+console.log(name.length);
+//console.log(name.toUppercase());
+console.log(name.charAt(5));
+console.log(name.trim('shaheed'));
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(sentence.toUpperCase());
+// Expected output: "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
+
+//console.log(name.toUpperCase());
+
+console.log(name.replace('shaheed','shfejkbsakjcdgjkabjkcbkjcbjksdb'))
+
+const url = "htttpsjnigaudgafcbgvauj/skbcagdkujf-43-495098"
+
+console.log(url.replace())
+
+
+const words =name.split('')
+console.log(words[3]);
